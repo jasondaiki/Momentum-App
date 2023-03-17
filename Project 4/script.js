ebf53f2142cb2ba55e2a5d8ea3853c6a9f08c388
jasondaiki <127101994+jasondaiki@users.noreply.github.com>
2023-03-17 18:50:16 +0800

    Update script.js

--- Project 4/script.js
+++ Project 4/script.js
@@ -4,17 +4,35 @@ const txt = "ENTER YOUR NAME"
 const speed = 200;
 const inputText = document.getElementById("inputtext");
 const submit = document.getElementById("submitone");
+const qoutes = document.getElementById("qoutebox");
 let form = document.getElementById("form");
 let input = document.getElementById("inputtext");
 let message = document.getElementById("maintext");
 let data = "";
 
 
-function changeBackground(){
-   let background = document.getElementById("wholepage");
+
+function changeBackgroundAndQoutes(){
+   const background = document.getElementById("wholepage");
+   const qoute = document.getElementById("qoutes");
    let backgroundImages = ["url(/img/1.jpg)", "url(/img/4.jpg)", "url(/img/2.jpg)"];
+   let qoutes = [
+    "“Whether here or in the real world, you can cry when it hurts. There’s no rule that you can’t show feelings just because it’s a game.” -Kirito",
+    "“Every day here is another one lost in the real world.” -Yuuki Asuna",
+    "“It’s impossible to work hard for something you don’t enjoy.” -Silica",
+    "“Stay alive you hear? I don’t care how you do it. Just stay alive until the end.” -Klein",
+    "“Even in a world like this, he was really living.” -Asuna",
+   ]
    let num = 0;
    let num_img = backgroundImages.length;
+   let num_qoute = qoutes.length;
+
+   setInterval(function(){
+    qoute.innerHTML = qoutes[num++];
+    if (num === num_qoute){
+        num = 0;
+    }
+   }, 15000);
 
    setInterval(function(){
     background.style.background = backgroundImages[num++];
@@ -24,7 +42,7 @@ function changeBackground(){
     }
    }, 15000);
 }
-changeBackground();
+changeBackgroundAndQoutes();
 
 function type(){
     if(i < txt.length){
@@ -43,9 +61,14 @@ form.addEventListener("submit", (e) => {
 
 submit.addEventListener("click", () =>{
     data = input.value;
-    message.innerHTML = "Welcome," + " " + data + "!";
+    let rtClock = new Date();
+    let hours = rtClock.getHours();
+    let greetings = ( hours > 12 ) ? "Good Afternoon" : ( hours > 18 ) ? "Good Evening" : "Good Morning";
+    message.innerHTML = greetings + "," + " " + data + "!";
     form.style.display = 'none';
     document.getElementById("clock").style.visibility = 'visible';
+    qoutes.style.visibility = 'visible';
+    
 })
 
 function realtimeClock(){
@@ -62,3 +85,76 @@ function realtimeClock(){
    hours + ":" + minutes + ":" + seconds + " " + amPM;
    let t = setTimeout(realtimeClock, 500);
 }
+
+function realDate(){
+    let date = new Date();
+    let days = date.getDay();
+    let months = date.getMonth();
+    let daysNum = date.getUTCDate();
+    let year = date.getFullYear();
+    let displayDate = document.getElementById("day");
+    let daysString = (days === 0) ? "Sunday" :
+    (days === 1) ? "Monday" :
+    (days === 2) ? "Tuesday" :
+    (days === 3) ? "Wednesday" :
+    (days === 4) ? "Thursday" :
+    (days === 5) ? "Friday" :
+    "Saturday";
+    let monthsString = (months === 0) ? "January" :
+    (months === 1) ? "February" :
+    (months === 2) ? "March" :
+    (months === 3) ? "April" :
+    (months === 4) ? "May" :
+    (months === 5) ? "June" :
+    (months === 6) ? "July" :
+    (months === 7) ? "August" :
+    (months === 8) ? "September" :
+    (months === 9) ? "October" :
+    (months === 10) ? "November" :
+    "December"
+    displayDate.innerHTML = daysString + " " + monthsString + " " + daysNum + "," + " " + year;
+}
+realDate();
+realtimeClock();
+
+function toDoApp(){
+    let newTask = document.getElementById("task");
+    let taskInput = document.getElementById("todoinput")
+    const addTask = document.getElementById("addtask");
+    const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+    const deleteBtn = document.getElementById("deleteBtn");
+
+    
+    
+    addTask.addEventListener("click", () =>{
+        itemsArray.push(taskInput.value)
+        localStorage.setItem("items", JSON.stringify(itemsArray))
+        location.reload()
+    })
+
+    function displayTask(){
+        let items = "";
+        for(let i = 0 ; i < itemsArray.length; i++){
+            items += `  <div id="newtask">
+                          <input type="checkbox" name="newtask">
+                          <label for="newtask">${itemsArray[i]}</label>
+                          <img class="edit" id="edit" src="/img/edit-xxl.png">
+                          <img class="delete" id="deleteBtn" onclick="deleteTask()" src="/img/delete-xxl.png">
+                        </div>`
+        }
+        newTask.innerHTML = items;
+        deleteBtn.addEventListener("click", deleteTask)
+   }
+
+   function deleteTask() {
+        itemsArray.splice(index, 1)
+        localStorage.setItem("items", JSON.stringify(itemsArray))
+        location.reload()
+   }
+    
+    displayTask()
+    
+    
+    
+}
+toDoApp()
